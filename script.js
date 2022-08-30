5fb6800b3560fbd8bb2c17c874ca7277f1bdfe38
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-08-30 12:56:45 +0200

    dom manipulacija sa jos elemenata

--- script.js
+++ script.js
@@ -3,3 +3,9 @@
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = 'Correct Number! 😁';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
