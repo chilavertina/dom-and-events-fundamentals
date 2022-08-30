ea706d827eb6ef7656f69df337688961d5de4a49
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-08-30 13:24:21 +0200

    prikazivanje random broja na stranici

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 */
 
 const number = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = number;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
