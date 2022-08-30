2bc3e87e823545ef1fa1e40e483f67ccc6acc1d6
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-08-30 19:10:23 +0200

    skracivanje koda

--- script.js
+++ script.js
@@ -15,24 +15,37 @@ let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highscore = 0;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+document.querySelector('.check').addEventListener(
+  'click',
+  function () {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess, typeof guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No Number! ❌';
-  } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent = 'Correct Number! 😁';
-    document.querySelector('.number').textContent = secretNumber;
+    if (!guess) {
+      document.querySelector('.message').textContent = 'No Number! ❌';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Correct Number! 😁';
+      document.querySelector('.number').textContent = secretNumber;
 
-    document.querySelector('body').style.backgroundColor = '#60b347';
-    document.querySelector('.number').style.width = '30rem';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
 
-    if (score > highscore) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      if (score > highscore) {
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+    } else if (guess !== secretNumber) {
+      document.querySelector('.message').textContent =
+        guess < secretNumber ? 'Too low! 🙁' : 'Too High! 🙁';
+      score = score - 1; //score--
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 😭';
+      document.querySelector('.score').textContent = 0;
     }
-  } else if (guess < secretNumber) {
+  }
+  /*
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too Low! 🙁';
       score = score - 1; //score--
@@ -50,8 +63,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'You lost the game! 😭';
       document.querySelector('.score').textContent = 0;
     }
-  }
-});
+  }*/
+);
 
 //CODING CHALLENGE 1
 
