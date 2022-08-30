10f851bd69cf3c9cd45271749ff00df72ff4e548
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-08-30 13:46:51 +0200

    definisanje score-a i umanjivanje score-a nakon svakog netacnog klika

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(document.querySelector('.guess').value);
 */
 
 const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -24,7 +25,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct Number! 😁';
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too Low! 🙁';
+    score = score - 1; //score--
+    document.querySelector('.score').textContent = score;
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too High! 🙁';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
