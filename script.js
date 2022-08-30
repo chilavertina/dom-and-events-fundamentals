26853a500f42700d02907f26625e61d55bf806a4
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-08-30 13:35:34 +0200

    promena poruke svaki put kad se ukuca pogresan broj

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
 
-const number = Math.trunc(Math.random() * 20 + 1);
-document.querySelector('.number').textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -20,5 +20,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number! ❌';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number! 😁';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low! 🙁';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High! 🙁';
   }
 });
