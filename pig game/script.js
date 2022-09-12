071d4553ef39b092eb48b22ddaf8786d3d29b0a5
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-12 12:31:34 +0200

    uklanjanje kocke u slucaju kraja igre

--- pig game/script.js
+++ pig game/script.js
@@ -56,6 +56,7 @@ btnHold.addEventListener('click', function () {
 
     if (scores[activePlayer] >= 100) {
       playing = false;
+      diceEl.classList.add('hidden');
       document
         .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
