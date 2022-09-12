47e8c45b2da5b5d6e088b462665ffdbdaa81d9fc
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-12 10:00:52 +0200

    selektiranje elemenata na dva razlicita nacina\

--- pig game/script.js
+++ pig game/script.js
@@ -1,1 +1,3 @@
 'use strict';
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1'); //getElementById je samo drugi nacin selekcije elementa (ista svrha kao i querySelector)
