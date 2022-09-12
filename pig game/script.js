ef2680171a94dff9bf4b4f187b75712738b07f78
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-12 10:09:19 +0200

    selektovanje elementa, stvaranje nove klase u css-u kako bi element u js mogao da bude 'hidden'

--- pig game/script.js
+++ pig game/script.js
@@ -1,3 +1,10 @@
 'use strict';
+
+//selektovanje elemenata
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); //getElementById je samo drugi nacin selekcije elementa (ista svrha kao i querySelector)
+const diceEl = document.querySelector('.dice');
+
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
