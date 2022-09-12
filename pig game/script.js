8d272adf2de52e328666cf1374409dbbc9af279d
Uroš Martić <104627712+chilavertina@users.noreply.github.com>
2022-09-12 11:18:05 +0200

    omogucavanje da se rezultat dodeljuje igracu koji trenutno igra sve dok ne dobije 1 tokom bacanja kocke

--- pig game/script.js
+++ pig game/script.js
@@ -15,7 +15,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //funkcionalnost za bacanje kocke
 btnRoll.addEventListener('click', function () {
@@ -26,7 +28,8 @@ btnRoll.addEventListener('click', function () {
 
   if (dice !== 1) {
     currentScore += dice; //currentScore = currentScore + dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
   }
 });
